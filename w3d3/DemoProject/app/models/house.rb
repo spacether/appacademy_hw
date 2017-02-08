ac63f07775c0b4a8022947ab9e59575c37dd3d64
Justin Black <justin.a.black@gmail.com>
2017-02-08 08:51:23 -0800

    Adds w3d3

---
+++ w3d3/DemoProject/app/models/house.rb
@@ -0,0 +1,10 @@
+class House < ActiveRecord::Base
+  validates :address, presence: true
+
+  has_many(
+    :people,
+    class_name: 'Person',
+    foreign_key: :house_id,
+    primary_key: :id
+  )
+end
